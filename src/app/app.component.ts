710ca8dde9850d32a47561420848f5a451bb62da
jos247car@gmail.com <45372826+josh002@users.noreply.github.com>
2026-04-14 18:43:55 -0300

    save product

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component, ViewChild } from '@angular/core';
 export class AppComponent {
   public appPages = [
     { title: 'Insumo', url: '/insumos', icon: 'file-tray-full-outline' },
-    { title: 'Cotización', url: '/cotizacion', icon: 'logo-usd' },
+    // { title: 'Cotización', url: '/cotizacion', icon: 'logo-usd' },
     { title: 'Productos', url: '/producto', icon: 'file-tray-stacked-outline' },
   ];
   constructor() { }
--- src/app/pages/producto/producto.page.ts
+++ src/app/pages/producto/producto.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { InsumoProducto, Producto } from 'src/app/interfaces/insumos';
 import { LocalStorage } from 'src/app/models/localStorage';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-producto',
@@ -8,29 +9,91 @@ import { LocalStorage } from 'src/app/models/localStorage';
   styleUrls: ['./producto.page.scss'],
 })
 export class ProductoPage implements OnInit {
-  list: Producto[] = [];
+  productList: Producto[] = [];
   productoNuevo: Producto = new Producto();
   listaInsumos: InsumoProducto[] = [];
-  constructor() {
+  totalProducto = 0;
+
+  constructor(private toastController: ToastController) {
     const items = localStorage.getItem(LocalStorage.productos);
     if (items) {
-      this.list = (JSON.parse(items) as Producto[]);
-    };
+      this.productList = JSON.parse(items) as Producto[];
+    }
     const insumos = localStorage.getItem(LocalStorage.insumos);
     if (insumos) {
-      this.listaInsumos = (JSON.parse(insumos) as InsumoProducto[]).map(insumo => {
-        insumo.cantidad = 0;
-        return insumo;
-      });
-    };
+      this.listaInsumos = (JSON.parse(insumos) as InsumoProducto[]).map(
+        (insumo) => {
+          insumo.cantidad = 0;
+          return insumo;
+        },
+      );
+    }
     console.log(this.listaInsumos);
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   guardarInsumosProductoNuevo() {
-    this.productoNuevo.insumos = this.listaInsumos.map((insumoProducto) =>
-      ({ nombre: insumoProducto.nombre, precio: insumoProducto.precio * insumoProducto.cantidad, cantidad: insumoProducto.cantidad }));
+    this.productoNuevo.insumos = this.listaInsumos.map((insumoProducto) => ({
+      nombre: insumoProducto.nombre,
+      precio: insumoProducto.precio * insumoProducto.cantidad,
+      cantidad: insumoProducto.cantidad,
+    }));
+
+    this.totalProducto = this.productoNuevo.insumos.reduce(
+      (total, insumo) => total + insumo.precio,
+      0,
+    );
+  }
+
+  saveProduct() {
+    if (!this.productoNuevo.nombre) {
+      return this.presentToast('Falta nombre');
+    }
+    if (this.productoNuevo.insumos.length === 0) {
+      return this.presentToast('Faltan productos');
+    }
+
+    if (!this.productList) {
+      this.productList = [];
+    }
+
+    const newProduct: Producto = {
+      nombre: this.productoNuevo.nombre,
+      insumos: this.productoNuevo.insumos,
+      precio: this.totalProducto,
+    };
+
+    this.productList.push(newProduct);
+    localStorage.setItem(
+      LocalStorage.productos,
+      JSON.stringify(this.productList),
+    );
+    this.reset();
+    this.presentToast('Producto agregado');
+  }
+
+  reset() {
+    this.productoNuevo = new Producto();
+    this.listaInsumos = [];
+    this.totalProducto = 0;
+  }
+
+  async presentToast(mensaje: string) {
+    const toast = await this.toastController.create({
+      message: mensaje,
+      duration: 2000,
+      position: 'top',
+    });
+    toast.present();
+  }
+
+  deleteProduct(producto: Producto) {
+    this.productList = this.productList.filter((itemList) => {
+      if (itemList.nombre === producto.nombre) {
+        return false;
+      }
+      return true;
+    });
   }
 }
